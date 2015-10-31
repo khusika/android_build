d38f3efb8b8ecf5826f45061c039fee135a4d6f0
Anonim47 <yosef@yosefadi.tk>
2015-10-31 14:30:16 +0700

    build: rename to xos_*
    Change-Id: Iafa5fd059612c065475989360f946f25aac81a15

--- envsetup.sh
+++ envsetup.sh
@@ -560,7 +560,7 @@ function breakfast()
             if [ -z "$variant" ]; then
                 variant="userdebug"
             fi
-            lunch cm_$target-$variant
+            lunch xos_$target-$variant
         fi
     fi
     return $?
